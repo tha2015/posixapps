1e0e533b25b42e1c86676d27777b4c4f8a081190
tha2015 <tha2015@users.noreply.github.com>
2019-02-03 12:24:55 -0800

    cygwin gtk

--- gtk/compile.sh
+++ gtk/compile.sh
@@ -4,6 +4,11 @@
 #sudo apt-get install pkg-config
 #sudo apt-get install libgtk-3-dev
 
+# cygwin
+# - install xorg-server and xinit
+# xinit -- -multiwindow -clipboard &
+# run app
+
 # gcc `pkg-config --cflags gtk+-3.0` -o hello main.c `pkg-config --libs gtk+-3.0`
 
 #make
