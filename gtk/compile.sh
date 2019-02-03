a624aa5a926a90e942cc0f12bfa34896c37bbb26
Thai H <tha2015@users.noreply.github.com>
2019-02-03 11:58:58 -0800

    update linux gtk

--- gtk/compile.sh
+++ gtk/compile.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env sh
 
-# gcc `pkg-config --cflags gtk+-3.0` -o example-0 example-0.c `pkg-config --libs gtk+-3.0`
+# linux
+#sudo apt-get install pkg-config
+#sudo apt-get install libgtk-3-dev
+
+# gcc `pkg-config --cflags gtk+-3.0` -o hello main.c `pkg-config --libs gtk+-3.0`
 
 #make
