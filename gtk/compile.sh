77fc9319f0dd1fa63f51cb03fad194705f5d53e1
tha2015 <tha2015@users.noreply.github.com>
2019-02-04 19:24:55 -0800

    updated

--- gtk/compile.sh
+++ gtk/compile.sh
@@ -9,6 +9,9 @@
 # xinit -- -multiwindow -clipboard &
 # run app
 
+# msys2
+# see http://blog.latepaul.com/glade-gtk-on-windows-with-msys2
+
 # gcc `pkg-config --cflags gtk+-3.0` -o hello main.c `pkg-config --libs gtk+-3.0`
 
 #make
