7a9f0b95b883f56958f059e3b0ef3ac46059b619
tha2015 <tha2015@users.noreply.github.com>
2019-02-04 19:37:22 -0800

    updated

--- gtk/main.c
+++ gtk/main.c
@@ -27,7 +27,7 @@ activate (GtkApplication *app,
   button_box = gtk_button_box_new (GTK_ORIENTATION_HORIZONTAL);
   gtk_container_add (GTK_CONTAINER (window), button_box);
 
-  button = gtk_button_new_with_label ("Hello World");
+  button = gtk_button_new_with_label ("Hello World2");
   g_signal_connect (button, "clicked", G_CALLBACK (print_hello), NULL);
   g_signal_connect_swapped (button, "clicked", G_CALLBACK (gtk_widget_destroy), window);
   gtk_container_add (GTK_CONTAINER (button_box), button);
